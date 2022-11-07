3d6d737f12479b9e9203f03ea21ca440b7756d40
David Earley <david.earley2@gmail.com>
2022-11-07 18:58:27 -0500

    Added and debugged time-based javascript functionality

--- javascript.js
+++ javascript.js
@@ -54,7 +54,7 @@ function playRound(playerSelection) {
             break;
         
         default:
-            result = "error";
+            result = "none";
     
         }
         const output = [playerSelection, computerSelection, result]
@@ -136,8 +136,13 @@ function resetGame(){
     gameNumber = 0;
     btns = document.querySelectorAll('.game-button');
     btns.forEach(btn => btn.classList.remove('selected'));
-    gameState = "play";
+    btns.forEach(btn => btn.classList.remove('hover'));
+    document.querySelector('#next').classList.add('hover');
+    document.querySelector('#start-next').textContent = 'Start Game';
+    gameState = "start";
     setDefaultMessages();
+
+    if (rpsInterval) clearInterval(rpsInterval);
 
  }
 
@@ -147,8 +152,12 @@ function resetGame(){
     const msgBox2 = document.querySelector('#result');
     const scoreBox = document.querySelector('#scores');
     scoreBox.textContent = `Game Number: ${gameNumber}. Your Score: ${playerScore}. Computer Score: ${computerScore}.`;
-    if (gameNumber==0) msgBox.textContent = 'Play against the computer.  First to 5 wins!';
+    if (gameNumber==0) msgBox.textContent = `Play against the computer.  First to ${MAX_SCORE} wins!`;
     if (gameNumber>0) msgBox.textContent = 'Choose a new selection';
+    
+    RPS.textContent = 'Click Start Game to Start!';
+
+    //document.querySelector('#start-next').textContent = 'Start Game';
 
     msgBox2.textContent = '';
 
@@ -171,6 +180,8 @@ function roundResultMessage(roundResult, scores){
             break;
         case "computer":
             msgBox.textContent = `The computer chose ${roundResult[1]}.  You lost this round!`;
+            break;
+        default:
             break;
 
         }
@@ -197,7 +208,7 @@ function updateScore(roundResult) {
 
 
 
-function checkWin(selection){
+function checkWin(computerSelection){
 
 
     if (gameState !== 'playerWin' && gameState !== 'computerWin') return;
@@ -212,95 +223,159 @@ function checkWin(selection){
         if (difference !== 1) plural = 's';
         
         document.querySelector('#next').classList.remove('hover');
+
+        if (gameState == 'computerWin' && computerSelection == 'none'){
+        msgBox.textContent = `Sorry! You forfeit this round to the computer.`
+        msgBox2.textContent = `The computer beat you by ${playerScore - computerScore} point${plural}`;
+    }
         
-        if (gameState == 'playerWin') {
-            msgBox.textContent = `Congratulations! The computer chose ${selection}.`
+        else if (gameState == 'playerWin') {
+            msgBox.textContent = `Congratulations! The computer chose ${computerSelection}.`
             msgBox2.textContent = `You beat the computer by ${playerScore - computerScore} point${plural}!`;
         }
         else if (gameState == 'computerWin'){
 
-        msgBox.textContent = `Sorry! The computer chose ${selection}.`
+        msgBox.textContent = `Sorry! The computer chose ${computerSelection}.`
         msgBox2.textContent = `The computer beat you by ${computerScore - playerScore} point${plural}. Reset to try again!`;
+
 
         }
     }
+
 }
+
+
+
+function handleEarly(){
+    clearInterval(rpsInterval);
+    RPS.textContent = 'Too Early!'
+    const msgBox = document.querySelector('#message')
+    msgBox.textContent = 'You forfeit this round to the computer.'
+    for (item of ['rock', 'paper', 'scissors']){
+        document.querySelector(`#${item}`).classList.remove('hover');
+    }
+    document.querySelector('#next').classList.add('hover');
+    document.querySelector('#reset').classList.add('hover');
+    computerScore ++;
+    roundResultMessage([,,], [playerScore, computerScore, gameNumber]);
+    
+    if (playerScore == MAX_SCORE) gameState = 'playerWin';
+    else if (computerScore == MAX_SCORE) gameState = 'computerWin';
+    else gameState = 'pause';
+    checkWin('none');
+}
+
+
+
+function handleLate(){
+    clearInterval(rpsInterval);
+    RPS.textContent = 'You waited too long!'
+    const msgBox = document.querySelector('#message')
+    msgBox.textContent = 'You forfeit this round to the computer.'
+    for (item of ['rock', 'paper', 'scissors']){
+        document.querySelector(`#${item}`).classList.remove('hover');
+    }
+    document.querySelector('#next').classList.add('hover');
+    document.querySelector('#reset').classList.add('hover');
+    computerScore ++;
+    roundResultMessage([,,], [playerScore, computerScore, gameNumber]);
+    if (playerScore == MAX_SCORE) gameState = 'playerWin';
+    else if (computerScore == MAX_SCORE) gameState = 'computerWin';
+    else gameState = 'pause';
+    
+    result = playRound('none');
+    checkWin('none');
+
+}
+
 
 
 
 function updateGame(selection){
     
-
-
-
-    document.querySelector('#reset').classList.add('hover');
-
-
-    if (selection == "reset") resetGame();
     
+    if (selection == 'start-next') selection = 'next';
 
-    else if(gameState=="pause"){
+    if (selection == "reset") {
+        resetGame();
+        return;
+    }
+
+    else if (gameState == 'early' && selection !== 'next'){
+        handleEarly();
+        return;
+    }
+
+    else if (gameState == 'late'){
+        handleLate();
+        return;
+    }
+
+    else if(gameState=="pause" || gameState == 'start'){
         if (selection !== "next") return;
         else if (selection == "next"){
+
             document.querySelectorAll('button').forEach(btn=> btn.classList.remove('selected'))
             document.querySelector('#next').classList.remove('hover');
             for (item of ['rock', 'paper', 'scissors']){
                 document.querySelector(`#${item}`).classList.add('hover')
             }
+            document.querySelector('#reset').classList.add('hover');
+            document.querySelector('#start-next').textContent = 'Next Round';
+        
             
             setDefaultMessages();
-            gameState = 'play';
+            animateRPS();
+
             }
         }
 
 
+
     else if (gameState=='play'){
 
+
         if(selection == 'rock' || selection == 'paper' || selection == 'scissors'){
+            
+
+            if(clearInterval(rpsInterval)) clearInterval(rpsInterval);
+            
             changeSelectedStyle(selection, "add"); 
             roundResult = playRound(selection);
             updateScore(roundResult);
+
             let scores = [playerScore, computerScore, gameNumber]
 
             roundResultMessage(roundResult, scores);
+        
 
             for (item of ['rock', 'paper', 'scissors']){
                 document.querySelector(`#${item}`).classList.remove('hover')
             }
             document.querySelector('#next').classList.add('hover');
 
-
-            if (playerScore == MAX_SCORE) gameState = 'playerWin';
-            else if (computerScore == MAX_SCORE) gameState = 'computerWin';
-            else gameState = 'pause';
-
-            checkWin(selection);
-
+        if (playerScore == MAX_SCORE) gameState = 'playerWin';
+        else if (computerScore == MAX_SCORE) gameState = 'computerWin';
+        else gameState = 'pause';
 
         }
-        
-    }
 
-    return;
-}
-
-
-
-
-
-
+        checkWin(roundResult[1]);
+    }}
+    
+    
 let playerScore = 0;
 let computerScore = 0;
 let gameNumber = 0;
-let gameState = 'play';
-let MAX_SCORE = 2;
-
-
-
+let gameState = 'start';
+let MAX_SCORE = 3;
+let MAX_WAIT = 2;
+let rpsInterval;
 
 
 
 const buttons = document.querySelectorAll('.game-button');
+const RPS = document.querySelector('.RPS');
 
 
 
@@ -310,10 +385,49 @@ buttons.forEach(btn => btn.addEventListener('click', getSelectionFromInput));
 const lowerResults = document.querySelector('#message');
 const lowerResults2 = document.querySelector('#scores');
 
-const RPS = document.querySelector('.RPS');
 
-document.querySelector('#next').classList.remove('hover');  //remove option for next button hover at start of game
-document.querySelector('#reset').classList.remove('hover');
+document.querySelector('#message').textContent = `Play against the computer. First to ${MAX_SCORE} wins!`;
+
+
+
+function animateRPS(){
+    const rpsOptions = ['Rock...', 'Paper...', 'Scissors...', 'Shoot!']
+    let rpsCounter = 0;
+    let lateCounter = 0;
+    exit = 0;
+    const msgBox = document.querySelector('#message')
+    let end = false;
+    gameState = 'early'
+
+    function updateRPS(){
+        RPS.textContent = rpsOptions[rpsCounter];
+
+        if (rpsCounter == rpsOptions.length-1 || end == true){
+            RPS.textContent = rpsOptions[rpsOptions.length-1];
+            end = true;
+            gameState = 'play';
+            lateCounter ++;
+        }
+
+        rpsCounter++;
+
+        if (lateCounter >= MAX_WAIT){
+            gameState = 'late';
+            RPS.textContent = 'You waited too long!';
+            msgBox.textContent = 'Computer wins this round.';
+            gameState = 'late';
+            updateGame('late');
+        } 
+    }
+
+    updateRPS();
+    rpsInterval = setInterval(updateRPS, 700);
+
+}
+
+
+
+
 
 
 
